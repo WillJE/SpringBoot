e9a5fd02c307211bded9b723f86b55f610836f70
will <wenlin7404@163.com>
2019-08-22 16:52:40 +0800

    commit

---
+++ IntelliJ-spring-boot-project/src/main/java/com/javaxiaozheng/SampleProjectIntelliJApplication.java
@@ -0,0 +1,12 @@
+package com.javaxiaozheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleProjectIntelliJApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleProjectIntelliJApplication.class, args);
+	}
+}
